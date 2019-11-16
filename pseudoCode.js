084c308d5cf13f52e37339438566eccd8d2537e4
Adena McGibbon <justadena@gmail.com>
2019-11-15 19:57:26 -0700

    Pseudo Code Outline

---
+++ pseudoCode.js
@@ -0,0 +1,75 @@
+//Goal: User can use the CLI to generate an HTML page that displays information about their team. 
+
+//create variables for the apps dependancies
+
+//propt the user to build an engineering team.
+  //team consists of a single manager and any number of engineers and inters
+
+// Team members' name (perhaps click the team members name to go to their git hub)
+// Prompt for member's EE ID#
+// different questions an intern vs a manager vs an engineer
+// Team members' email address (hyperlinked)
+// Team members' github URL(hyperlinked)
+// Need a method to check if the user is a manager??
+
+// No limit to the number of team members that can be input
+// three types of team members
+
+//Employee Class
+  //this will contain all of the characteristics that each EE type has in common
+  //name
+  //id
+  //title
+  //getName()
+  //getId()
+  //getEmail()
+  //getRole() returns EE
+  //projectName()??  if the project name is the same as other team members, display on same page?
+
+
+//Manager Class
+ //extends employee
+ //office number what about name
+ //getRole() overridden to return Manager
+ 
+
+//Engineer Class
+  //extends employee
+  //getGithub()
+  //github username
+  //getRole() overridden to return Engineer
+
+
+//Intern class
+  //extends employee
+  //school
+  //length of internship
+  //school
+  //getSchool()
+  //getRole() Overridden to return Intern
+
+//generate a team HTML in the output directory
+  //display a team roster
+  //each team member must have 
+    //name
+    //role
+    //id
+    //role specific property (office #, github profile, office number)
+
+
+
+
+
+
+
+//1. npm run test
+//2. Create or update classes to pass a single test case
+//3. Repeat
+
+// make methods simple so they are easy to test
+// 
+
+
+
+
+
