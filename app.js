49137d487c69d58f765a4bab2d475ac75cb81429
Adena McGibbon <justadena@gmail.com>
2019-11-18 23:03:51 -0700

    Switch case and prompt for different team member roles is working

--- app.js
+++ app.js
@@ -4,78 +4,159 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-const team = [];
-const teamIDs = [];
-
+const team = [ ];
+const teamIDs = [ ];
+let addToTeam = true;
 
 // Since every team will have one manager, I'll first prompt the user to add a manager 
-function promptForManager() {
+function managerInfo() {
+  return inquirer
+    .prompt([{
+      type: "input",
+      message: "Please enter the manager's name:",
+      name: "name"
+    }, {
+      type: "input",
+      message: "Please enter the manager's id#:",
+      name: "id"
+    }, {
+      type: "input",
+      message: "Please enter the manager's email:",
+      name: "email"
+    }, {
+      type: "input",
+      message: "What is the manager's office number:",
+      name: "officeNumber"
+    }
+    ])
+}
+
+// I want to prompt the user for the type of team member they want to add
+async function roleInfo() {
+  return inquirer.prompt([{
+    type: "list",
+    name: "role",
+    message: `Please select the next team member's role.  If you are finished adding team members, selct 'Build Team'`,
+    choices: [
+      "Engineer",
+      "Intern",
+      "Build Team!"
+    ]
+  }])
+}
+
+function engineerPrompt() {
   return inquirer
   .prompt([{
     type: "input",
-    message: "Please enter the team manager's first and last name.",
+    message: "Please enter the engineer's name:",
     name: "name"
   }, {
     type: "input",
-    message: "Please enter the manager's id#",
+    message: "Please enter the engineer's ID:",
     name: "id"
   }, {
     type: "input",
-    message: "Please enter the manager's email",
+    message: "Please enter the engineer's email address:",
     name: "email"
+  }
+  , {
+    type: "input",
+    message: "What is the engineer's GitHub username?",
+    name: "github"
+  }
+  ])
+}
+
+function internPrompt() {
+  return inquirer
+  .prompt([{
+    type: "input",
+    message: "Please enter the intern's name:",
+    name: "name"
   }, {
     type: "input",
-    message: "What is the manager's office number",
-    name: "officeNumber"
+    message: "Please enter the intern's ID:",
+    name: "id"
+  }, {
+    type: "input",
+    message: "Please enter the intern's email address:",
+    name: "email"
   }
-  ])  
+  , {
+    type: "input",
+    message: "Which school do you attend?",
+    name: "school"
+  }
+  ])
 }
 
 // Build an instance of the manager based on their response
-promptForManager()
-  .then(function(res) {
-    manager = new Manager(
+console.log(`Welcome to the team building excercise!
+Let's start with the manager...`);
+
+managerInfo()
+  .then(function (res) {
+    manager = new Manager (
       res.name,
       res.id,
       res.email,
-      res.role,
       res.officeNumber,
-      );
-  
+      res.role
+    );
+      
     team.push(manager);
-    teamIDs.push(manager.id)
-    console.log(team);
-    console.log(teamIDs);
+    teamIDs.push(manager.id);
+    
+    console.log(`Great! The manager has been added to the team.
+    `);
+    roleInfo()
+    .then(function (res) {
+      switch (res.role) {
+        case "Engineer" :
+        engineerPrompt()
+        .then(function(res) {
+          engineer = new Engineer ( 
+            res.name,
+            res.id,
+            res.email,
+            res.github,
+            res.role
+          )
+          team.push(engineer);
+          teamIDs.push(engineer.id);
+        })
+       break; 
+       case "Intern" :
+        internPrompt()
+        .then(function(res) {
+          intern = new Intern (
+            res.name,
+            res.id,
+            res.email,
+            res.school,
+            res.role
+          )
+          team.push(intern);
+          teamIDs.push(intern.id);
+          console.log(team);
+          })
+          break;
+          case "Build Team!" :
+            console.log(team)
+        }
+      })
   })
 
-// Now that the manager is saved to the empty array, I want to 
   
 
   
+// Since the user can enter an infinite number of interns and engineers to their team, I need to loop through roleInfo until user is finished evaluates to true.
 
-// createTeamMember(); {
-//   return inquirer.prompt([{
-//     type: "input",
-//     name: "Manager Info",
-//     message: "Please enter the team Manager's first and last name",
-//     choices: [
-//       "Engineer", 
-//       "Intern",
-//       ],
-//     default: "Employee"
-//   }
-//   ]); 
-// }
 
-// I want to prompt the user for the type of team member they want to add
 
 // Build an instance of a team member based on their response
 
-// The selection of manager means that I'll build a new manager
-
-// The seletion of engineer means I'll build a new engineer
-
-// the selection of intern means I'll build a new intern
 
 // after they've selected the type team member they want to add I need to ask whether they want to add an
 
